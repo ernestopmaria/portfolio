dd808a19839e1bc08bd2c3d63001244ae23e4a02
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-06-18 16:57:12 +0100

    starting to build my portfolio

---
+++ myportfolio/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <div className ="grid grid-cols-12 gap-6">
+    <div className="col-span-3 bg-white">sidebar</div>
+    <div className="col-span-9 bg-white">
+    <Component {...pageProps} />
+    </div>
+
+  </div>
+  
+
+}
+
+export default MyApp
---
+++ myportfolio/pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+const index =()=>{
+  return (
+    <div>
+<h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default index
